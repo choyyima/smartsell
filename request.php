bf076a8b9016be23a08bea20f8eaf78e3116408f
choyyima <choyyima.aja@gmail.com>
2016-06-15 22:38:30 +0700

    new file function on index

---
+++ request.php
@@ -0,0 +1,21 @@
+<div class="main-content-inner">
+    <div class="breadcrumbs" id="breadcrumbs">
+        <script type="text/javascript">
+            try {
+                ace.settings.check('breadcrumbs', 'fixed')
+            } catch (e) {
+            }
+        </script>
+
+        <ul class="breadcrumb">
+            <li>
+                <i class="ace-icon fa fa-home home-icon"></i>
+                <a href="#">Home</a>
+            </li>
+            <li class="active">Request Deposit</li>
+        </ul><!-- /.breadcrumb -->
+
+    </div>
+
+
+</div>
